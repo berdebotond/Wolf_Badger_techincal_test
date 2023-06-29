91ac2f7e94e05af8ba4e6f6a21fd1d782881fc08
Botond Berde <botondberde@gmail.com>
2023-06-29 13:47:36 +0100

    add config

---
+++ modules/config/config.py
@@ -0,0 +1,28 @@
+"""
+Config module.
+"""
+import dataclasses
+import os
+import uuid
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+
+@dataclasses.dataclass
+class Config:
+    LOG_LEVEL: str = os.getenv("LOG_LEVEL", "DEBUG")
+    APP_DEBUG: bool = os.getenv("APP_DEBUG", True)
+    APP_HOST: str = os.getenv("APP_HOST", "0.0.0.0")
+    APP_PORT: int = int(os.getenv("APP_PORT", 5000))
+    GITHUB_CLIENT_ID: str = os.getenv("GITHUB_CLIENT_ID", "")
+    GITHUB_CLIENT_SECRET: str = os.getenv("GITHUB_CLIENT_SECRET", "")
+    GITHUB_REDIRECT_URI: str = os.getenv("GITHUB_REDIRECT_URI", "http://localhost:5000/github/callback")
+    GITHUB_API_BASEURL: str = "https://api.github.com"
+    # generate a UUID for the secret key
+    APP_SECRET_KEY: str = os.getenv("API_SECRET_KEY", str(uuid.uuid4()))
+    # from compose file  for prostiqsql
+    DB_URI: str = os.getenv("DB_URI", 'postgresql://postgres:example@localhost:5432/postgres')
+
+
+cfg = Config()
